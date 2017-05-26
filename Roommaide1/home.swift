0bdac25939ce0678db1171931ba2f89e2bf92109
gilabraham <galakers24@aol.com>
2017-05-26 15:57:00 -0400

    test

--- Roommaide1/home.swift
+++ Roommaide1/home.swift
@@ -18,7 +18,7 @@ class home: UIViewController {
         super.viewDidLoad()
         
         //make background pic blury
-        // test
+        //  test
         insertBlurView(banner, style: UIBlurEffectStyle.dark)
         //insertBlurView(uiView, style: UIBlurEffectStyle.dark)
 
